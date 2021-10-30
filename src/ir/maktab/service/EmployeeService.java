eba379c5f80215baade79618eaf7323ec5c1826f
nasim <zamaninasim213@gamail.com>
2021-10-30 23:33:21 +0330

    add call method of dao for use in main

--- src/ir/maktab/service/EmployeeService.java
+++ src/ir/maktab/service/EmployeeService.java
@@ -1,4 +1,23 @@
 package ir.maktab.service;
 
+import ir.maktab.dao.EmployeeDao;
+import ir.maktab.model.Employee;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class EmployeeService {
+    EmployeeDao employeeDao = new EmployeeDao();
+    int thisYear = 1400;
+
+    public EmployeeService() throws SQLException, ClassNotFoundException {
+    }
+
+    public List<Employee> getEmployees() throws SQLException {
+        return employeeDao.readEmployeesFromDataBase();
+    }
+
+    public List<Integer> returnFindAllAvailableYears() throws SQLException {
+        return employeeDao.FindAllAvailableYears(thisYear);
+    }
 }
