5733d62d320337dcc4ab2000ffdcb09aa94ff401
nasim <zamaninasim213@gamail.com>
2021-10-30 23:24:43 +0330

    add employee class and property

---
+++ src/ir/maktab/model/Employee.java
@@ -0,0 +1,9 @@
+package ir.maktab.model;
+
+public class Employee {
+    private Integer id;
+    private String fullName;
+    private Integer enteringYear;
+    private Integer salaryLimits;
+    private Integer personalId;
+}
