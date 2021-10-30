dfb5ccf41f7bc811a4d1760621a69ae08df19701
nasim <zamaninasim213@gamail.com>
2021-10-30 23:34:53 +0330

    override comper to in employee class

--- src/ir/maktab/model/Employee.java
+++ src/ir/maktab/model/Employee.java
@@ -5,10 +5,19 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class Employee {
+public class Employee implements Comparable<Employee> {
     private Integer id;
     private String fullName;
     private Integer enteringYear;
     private Integer salaryLimits;
     private Integer personalId;
+
+    @Override
+    public int compareTo(Employee o) {
+        if (this.enteringYear == o.enteringYear)
+            return Integer.compare(this.salaryLimits, o.salaryLimits);
+        else if (this.enteringYear < o.enteringYear)
+            return 1;
+        else return -1;
+    }
 }
